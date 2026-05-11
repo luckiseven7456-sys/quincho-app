5651c166657e2b26b1234fcc0087176d48f8bd1e
Lucas <Luckiseven7456@gmail.com>
2026-05-11 15:16:27 -0300

    firebase credentials

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const admin = require("firebase-admin");
 const mercadopago = require("mercadopago");
 
-admin.initializeApp();
+const serviceAccount = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
 app.use(express.json());
